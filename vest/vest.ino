d5ac7866bf84c9adf9452cfaeb89c74cdbcd8438
Yuval Kohavi <yuval.kohavi@gmail.com>
2015-08-16 10:17:38 -0400

    added config mode

--- vest/vest.ino
+++ vest/vest.ino
@@ -5,11 +5,15 @@
 
 #include <TimerOne.h>
 
-#define  DEBUG 1
+// #define  DEBUG 1
 
 #define CASSERT(X) typedef byte STATIC_ASSERT[X?1:-1]
 
+
+#ifdef DEBUG
 #define LEDPIN 13
+#endif
+
 #define PIEZO_PIN 9
 #define NUM_PIXELS 8
 #define LED_STRIP_PIN 5
@@ -37,6 +41,7 @@ int octaveExp = 0;
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 const int NUM_PINS = ARRAY_SIZE(pins);
+
 // static assert to make sure i didn't miss anything
 CASSERT(NUM_PINS == ARRAY_SIZE(names) );
 CASSERT(NUM_PINS == ARRAY_SIZE(tones) );
@@ -85,6 +90,10 @@ uint32_t Wheel(byte WheelPos) {
   }
 }
 
+// Todo:
+// 1. add mute buttons
+// 2. and rainbow
+// 3.
 
 void setup() {
 #ifdef DEBUG
@@ -96,9 +105,10 @@ void setup() {
   // init led strip
   strip.begin();
 
+#ifdef DEBUG
   pinMode(LEDPIN, OUTPUT);
+#endif
   pinMode(PIEZO_PIN, OUTPUT);
-
 
   // read unconnected analog pin
   randomSeed(analogRead(6));
@@ -123,14 +133,19 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
 
   // three beeps, all ok
+
+#ifdef DEBUG
   digitalWrite(LEDPIN, HIGH);
-  piezo.play(329, 200);
+#endif
+  piezo.play(tones[4], 200);
   delay(100);
-  piezo.play(329, 200);
+  piezo.play(tones[4], 200);
   delay(100);
-  piezo.play(329, 250);
+  piezo.play(tones[4], 250);
   delay(150);
+#ifdef DEBUG
   digitalWrite(LEDPIN, LOW);
+#endif
 
 
 #ifdef DEBUG
@@ -145,16 +160,17 @@ char magics[][5] = {
   {'c', 'C', 'C', 'C', 'C'},
   {'c', 'c', 'c', 'c', 'd'},
   {'d', 'd', 'd', 'd', 'c'},
+  {'c', 'd', 'd', 'd', 'd'},
 };
-Magicfunc magicfuncs[] = { doMagic1, doMagic2, doMagic3};
+Magicfunc magicfuncs[] = { doMagic1, doMagic2, doMagic3, doMagic4};
 
 CASSERT(ARRAY_SIZE(magicfuncs) == ARRAY_SIZE(magics) );
 
 void randomizeStrip() {
-  
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, random(0xFFFFFF));
-      strip.show();
+
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, random(0xFFFFFF));
+    strip.show();
   }
 }
 
@@ -167,35 +183,40 @@ void doMagic1() {
 
   for (int i = 0; i < length; i++) {
     randomizeStrip();
-    byte time = times[i] - '0';
-    byte note = melody[i];
-    int freq = 0;
-    int octave = 0;
-    for (int j = 0; j < sizeof(names) / sizeof(names[0]); j++) {
-      if (names[j] == note) {
-        freq = tones[j];
-        octave = octaves[j] - '0' - 5;
-      }
-    }
-
-    if (freq != 0 || note == 'p') {
-
-      if (freq > 0) {
-        if (octave >= 0) {
-          freq <<= octave;
-        } else {
-          freq >>= (-octave);
-        }
-        piezo.start(freq);
-      }
-      delay(tempo * time);
-      piezo.stop();
-    } else {
-      // invalid note!
-    }
-
+    playStep(melody, times, octaves, i, tempo);
   }
 
+}
+
+
+void playStep(byte* melody, byte* times, byte* octaves, int i, int tempo) {
+
+  byte timeLength = times[i] - '0';
+  byte note = melody[i];
+  int freq = 0;
+  int octave = 0;
+  for (int j = 0; j < sizeof(names) / sizeof(names[0]); j++) {
+    if (names[j] == note) {
+      freq = tones[j];
+      octave = octaves[j] - '0' - 5;
+    }
+  }
+
+  if (freq != 0 || note == 'p') {
+
+    if (freq > 0) {
+      if (octave >= 0) {
+        freq <<= octave;
+      } else {
+        freq >>= (-octave);
+      }
+      piezo.start(freq);
+    }
+    delay(tempo * timeLength);
+    piezo.stop();
+  } else {
+    // invalid note!
+  }
 
 }
 
@@ -207,7 +228,10 @@ void doMagic3() {
   octaveExp--;
 }
 
-
+void doMagic4() {
+  // TODO animate
+  // 8 pixel
+}
 
 int addNote(char note) {
 
@@ -217,7 +241,6 @@ int addNote(char note) {
 
   arr[arrEndIndex] = note;
   // get begining index:
-
 
 #ifdef DEBUG
   Serial.print("magic arr:");
@@ -250,16 +273,97 @@ int addNote(char note) {
 
 int doLightShow(int special) {
   // play magic mode intro
-  piezo.play(329, 200);
+  piezo.play(tones[4], 200);
   delay(100);
-  piezo.play(329, 200);
+  piezo.play(tones[4], 200);
   delay(100);
-  piezo.play(329, 250);
+  piezo.play(tones[4], 250);
 
   magicfuncs[special]();
   return 0;
 }
 
+void colorWipe(uint32_t c) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+}
+
+inline int getnote(int i) {
+  return (octaveExp < 0) ? tones[i] >> -octaveExp : tones[i] <<  octaveExp;
+}
+
+
+bool mute = false;
+bool lightsoff = false;
+
+
+const uint32_t red = strip.Color(255, 0, 0);
+const uint32_t green = strip.Color(0, 255, 0);
+const uint32_t blu = strip.Color(0, 0, 255);
+const uint32_t yellow = strip.Color(255, 255, 0);
+
+bool configCurrentPressed = false;
+void doConfig() {
+
+  if (piezo.isOn()) {
+    piezo.stop();
+  }
+  // the strip goes red in config mode
+
+  colorWipe(yellow);
+
+  int i;
+  for (i = 5; i < NUM_PINS; ++i) {
+    if (isPressed(pins[i])) {
+      break;
+    }
+  }
+
+  bool anyButtonPressed = (i < NUM_PINS);
+  if (anyButtonPressed && (!configCurrentPressed)) {
+    // do something
+    //   0    1    2    3    4    5    6    7    8    9   10   11
+    // {'c', 'C', 'd', 'D', 'e', 'f', 'F', 'g', 'G', 'a', 'A', 'b'};
+    switch (i) {
+      case 1:
+        mute = !mute;
+        if (mute) {
+          colorWipe(red);
+        } else {
+          colorWipe(green);
+          piezo.play(getnote(0), 200);
+        }
+        delay(500);
+        break;
+      case 3:
+        lightsoff = !lightsoff;
+        if (lightsoff) {
+          colorWipe(0);
+        } else {
+          colorWipe(yellow);
+        }
+        delay(500);
+        break;
+      case 5:
+        octaveExp--;
+        piezo.play(getnote(0), 200);
+        break;
+      case 7:
+        octaveExp++;
+        piezo.play(getnote(0), 200);
+        break;
+
+
+    }
+
+  } else if ((!anyButtonPressed) && (configCurrentPressed)) {
+    configCurrentPressed = false;
+  }
+
+
+}
 
 bool currentPressed = false;
 char currentNote = '\0';
@@ -272,6 +376,26 @@ void loop() {
       break;
     }
   }
+  if (i == 0) {
+    // check if we are in config mode
+    if (isPressed(pins[2]) && isPressed(pins[4])) {
+      doConfig();
+      return;
+    }
+
+  }
+
+  if (!mute) {
+    playSounds(i);
+  }
+
+  if (!lightsoff) {
+    animate();
+  }
+}
+
+void playSounds(int i) {
+
 
   bool anyButtonPressed = (i < NUM_PINS);
 
@@ -291,17 +415,17 @@ void loop() {
 #ifdef DEBUG
       Serial.print("Note index: ");
       Serial.println(i);
+      digitalWrite(LEDPIN, HIGH);
 #endif
       currentNote = names[i];
       currentPressed = true;
       noteIndex = i;
-      digitalWrite(LEDPIN, HIGH);
       int special = addNote(currentNote);
 
       if (special == -1) {
 
 
-        piezo.start((octaveExp < 0) ? tones[i] >> -octaveExp : tones[i] <<  octaveExp);
+        piezo.start(getnote(i));
       } else {
         doLightShow(special);
       }
@@ -310,24 +434,16 @@ void loop() {
   } else if ((!anyButtonPressed) && (currentPressed)) {
 #ifdef DEBUG
     Serial.println("Note OFF");
+    digitalWrite(LEDPIN, LOW);
 #endif
     // key was released (no touch, and it was pressed before)
     currentPressed = false;
     noteIndex = -1;
     currentNote = '\0';
     piezo.stop();
-    digitalWrite(LEDPIN, LOW);
   }
-
-  animate();
 }
 
-
-#ifdef DEBUG
-const uint32_t red = strip.Color(255, 0, 0);
-const uint32_t blu = strip.Color(0, 0, 255);
-const uint32_t yellow = strip.Color(255, 255, 0);
-#endif
 
 unsigned long lastDrawTime = 0;
 unsigned long stepIndex = 0;
